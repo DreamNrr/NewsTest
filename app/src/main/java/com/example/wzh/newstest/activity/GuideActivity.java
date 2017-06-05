e74afddd33a8a938d4df00a80e982bd07efbd024
DreamNrr <wzh74371233@163.com>
2017-06-05 20:26:47 +0800

    红点移动

--- app/src/main/java/com/example/wzh/newstest/activity/GuideActivity.java
+++ app/src/main/java/com/example/wzh/newstest/activity/GuideActivity.java
@@ -6,11 +6,14 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import com.example.wzh.newstest.R;
+import com.example.wzh.newstest.utils.DensityUtil;
 
 import java.util.ArrayList;
 
@@ -25,6 +28,11 @@ public class GuideActivity extends AppCompatActivity {
     Button btnStartMain;
     @InjectView(R.id.activity_guide)
     RelativeLayout activityGuide;
+    @InjectView(R.id.ll_point_group)
+    LinearLayout llPointGroup;
+    @InjectView(R.id.iv_red_point)
+    ImageView ivRedPoint;
+    private int leftMargin;
 
     private ArrayList<ImageView> imageViews;
     private int[] images = {R.drawable.guide_1, R.drawable.guide_2, R.drawable.guide_3};
@@ -40,20 +48,43 @@ public class GuideActivity extends AppCompatActivity {
 
     private void initData() {
         imageViews = new ArrayList<>();
-        for(int i = 0; i < images.length;i++) {
+        for (int i = 0; i < images.length; i++) {
             ImageView imageView = new ImageView(this);
             imageView.setBackgroundResource(images[i]);
             imageViews.add(imageView);
+            //三个灰点
+            ImageView point = new ImageView(this);
+            point.setImageResource(R.drawable.guide_point_normal);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(DensityUtil.dip2px(GuideActivity.this, 10), DensityUtil.dip2px(GuideActivity.this, 10));
+            point.setLayoutParams(params);
+            if (i != 0) {
+                params.leftMargin = DensityUtil.dip2px(GuideActivity.this, 10);
+            }
+            //添加到线性布局
+            llPointGroup.addView(point);
         }
         vp.setAdapter(new MyPagerAdapter());
         vp.addOnPageChangeListener(new MyOnPageChangeListener());
+        ivRedPoint.getViewTreeObserver()
+                .addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                ivRedPoint.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                leftMargin = llPointGroup.getChildAt(1).getLeft() - llPointGroup.getChildAt(0).getLeft();
+            }
+        });
 
     }
-    class MyOnPageChangeListener implements ViewPager.OnPageChangeListener{
+
+    class MyOnPageChangeListener implements ViewPager.OnPageChangeListener {
         @Override
         public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
+            float left = leftMargin *(positionOffset + position) ;
+            RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) ivRedPoint.getLayoutParams();
+            params.leftMargin = (int) left;
+            ivRedPoint.setLayoutParams(params);
         }
+
         //选中某个页面是回调
         @Override
         public void onPageSelected(int position) {
@@ -65,6 +96,7 @@ public class GuideActivity extends AppCompatActivity {
                 btnStartMain.setVisibility(View.GONE);
             }
         }
+
         @Override
         public void onPageScrollStateChanged(int state) {
 
@@ -72,25 +104,24 @@ public class GuideActivity extends AppCompatActivity {
     }
 
 
-
-
-
-
-    class MyPagerAdapter extends PagerAdapter{
+    class MyPagerAdapter extends PagerAdapter {
         @Override
         public int getCount() {
             return imageViews.size();
         }
+
         @Override
         public boolean isViewFromObject(View view, Object object) {
             return view == object;
         }
+
         @Override
         public Object instantiateItem(ViewGroup container, int position) {
             ImageView imageView = imageViews.get(position);
             container.addView(imageView);
             return imageView;
         }
+
         @Override
         public void destroyItem(ViewGroup container, int position, Object object) {
             container.removeView((View) object);
---
+++ app/src/main/java/com/example/wzh/newstest/utils/DensityUtil.java
@@ -0,0 +1,22 @@
+package com.example.wzh.newstest.utils;
+
+import android.content.Context;
+
+/**
+ * Created by WZH on 2017/6/5.
+ */
+
+public class DensityUtil {
+    public static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+     */
+    public static int px2dip(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+}
