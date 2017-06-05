1cbd2d5e17aa06652865d2280366326bd5c951c3
DreamNrr <wzh74371233@163.com>
2017-06-05 19:17:14 +0800

    引导页面显示图片

--- app/src/main/java/com/example/wzh/newstest/activity/GuideActivity.java
+++ app/src/main/java/com/example/wzh/newstest/activity/GuideActivity.java
@@ -1,12 +1,18 @@
 package com.example.wzh.newstest.activity;
 
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import com.example.wzh.newstest.R;
+
+import java.util.ArrayList;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -19,6 +25,8 @@ public class GuideActivity extends AppCompatActivity {
     Button btnStartMain;
     @InjectView(R.id.activity_guide)
     RelativeLayout activityGuide;
+
+    private ArrayList<ImageView> imageViews;
     private int[] images = {R.drawable.guide_1, R.drawable.guide_2, R.drawable.guide_3};
 
     @Override
@@ -31,6 +39,38 @@ public class GuideActivity extends AppCompatActivity {
     }
 
     private void initData() {
+        imageViews = new ArrayList<>();
+        for(int i = 0; i < images.length;i++) {
+            ImageView imageView = new ImageView(this);
+            imageView.setBackgroundResource(images[i]);
+            imageViews.add(imageView);
+        }
+        vp.setAdapter(new MyPagerAdapter());
+
+    }
+    class MyPagerAdapter extends PagerAdapter{
+
+        @Override
+        public int getCount() {
+            return imageViews.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            ImageView imageView = imageViews.get(position);
+            container.addView(imageView);
+            return imageView;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
     }
 
     @OnClick(R.id.btn_start_main)
